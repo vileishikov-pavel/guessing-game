4edeb8404660debe44ce5984ab7c2a2458609a44
Pavel Vileishikov <pvileyshikov@resilio.com>
2017-09-25 00:58:38 +0300

    first try

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.range = [];
+        for(min; min <= max; min++) {
+            this.range.push(min);
+        }
     }
 
     guess() {
-
+        this.index = Math.round(this.range.length / 2);
+        return this.range[this.index];
     }
 
     lower() {
-
+        this.range = this.range.slice(0, this.index);
     }
 
     greater() {
-
+        this.range = this.range.slice(this.index, this.range.length);
     }
 }
 
